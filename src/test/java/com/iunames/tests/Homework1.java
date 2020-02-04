6c7fbc940ba04c98d4f709f2c1d7cc2c9ecb066e
fatmalemdaroglu <fatmalemdaroglu@icloud.com>
2020-02-03 18:30:40 -0800

    first commit

---
+++ src/test/java/com/iunames/tests/Homework1.java
@@ -0,0 +1,108 @@
+package com.iunames.tests;
+
+import com.iunames.utilities.ApiUtilities;
+import com.iunames.utilities.ConfigurationReader;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class Homework1 {
+
+    @BeforeAll
+    public static void setup() {
+        baseURI = ConfigurationReader.getProperty("baseURI");
+    }
+
+    @Test
+    public void NoParamTest() {
+        given().get().
+                then().assertThat().statusCode(200).
+                contentType("application/json; charset=utf-8").
+                body("name", not(empty())).
+                body("surname", not(empty())).
+                body("gender", notNullValue()).
+                body("region", notNullValue()).
+                log().body(true);
+
+    }
+
+    @Test
+    public void GenderTest() {
+        given().queryParam("gender", "female").get().
+                then().assertThat().statusCode(200).
+                contentType("application/json; charset=utf-8").
+                body("gender", is("female")).
+                log().all(true);
+    }
+
+    @Test
+    public void TwoParamTest() {
+        given().queryParam("gender", "female").
+                queryParam("region", "Turkey").get().
+                then().assertThat().statusCode(200).
+                contentType("application/json; charset=utf-8").
+                body("gender", is("female")).
+                body("region", is("Turkey")).
+                log().all(true);
+    }
+
+    @Test
+    public void InvalidGenderTest() {
+        Response response = given().queryParam("gender", "unisex").get();
+        response.then().assertThat().statusCode(400).
+                statusLine("HTTP/1.1 400 Bad Request").
+                contentType("application/json; charset=utf-8").
+                body("error", containsString("Invalid gender")).
+                log().all(true);
+
+    }
+
+    @Test
+    public void InvalidRegionTest() {
+        Response response = given().queryParam("region", "female").get();
+        response.then().assertThat().statusCode(400).
+                statusLine("HTTP/1.1 400 Bad Request").
+                contentType("application/json; charset=utf-8").
+                body("error", containsString("Region or language not found")).
+                log().all(true);
+    }
+    @Test
+    public void AmountAndRegionTest() {
+        Response response = given().queryParam("region", "Turkey").
+                queryParam("amount", "20").get();
+        response.then().assertThat().statusCode(200).
+                contentType("application/json; charset=utf-8").
+                log().all(true);
+
+        List<Object> listOfName = response.jsonPath().get();
+        boolean hasDuplicates = ApiUtilities.hasDuplicates(listOfName);
+        assertFalse(hasDuplicates, "List has some duplicates");
+
+    }
+
+    @Test
+    public void ThreeParamsTest() {
+        given().queryParam("region", "Turkey").
+                queryParam("gender", "female").
+                queryParam("amount", "2").get().
+                then().assertThat().statusCode(200).
+                                    contentType("application/json; charset=utf-8").
+                                    body("region", everyItem(is("Turkey"))).
+                                    body("gender", everyItem(is("female"))).
+                        log().all(true);
+    }
+
+    @Test
+    public void AmountCountTest() {
+        given().queryParam("amount", "5").get().
+                then().assertThat().statusCode(200).
+                contentType("application/json; charset=utf-8").
+                body("",hasSize(5)).
+                        log().all(true);
+    }
+}
---
+++ src/test/java/com/iunames/utilities/ConfigurationReader.java
@@ -0,0 +1,27 @@
+package com.iunames.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties configFile;
+
+    static{
+        try{
+            FileInputStream fileInputStream = new FileInputStream("Configuration.properties");
+            configFile =new Properties();
+            configFile.load(fileInputStream);
+            fileInputStream.close();
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key){
+        return configFile.getProperty(key);
+    }
+
+}
